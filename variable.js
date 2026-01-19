ece82810f7957d43868e86e9bc4b7de54a370a32
Saumy003 <saumysingh2005@gmail.com>
2026-01-19 23:24:23 +0530

    variables in js

---
+++ variable.js
@@ -0,0 +1,12 @@
+fullName = "JavaScript";
+price = "$99.99";
+age = 19;
+x = null;
+y = undefined;
+isEating = true;
+
+
+console.log(fullName);
+console.log(age);
+console.log(price);
+console.log(isEating);
