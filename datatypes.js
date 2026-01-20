a8ac712b227907e0325005ea9747ead8a4bbbf22
Saumy003 <saumysingh2005@gmail.com>
2026-01-20 22:56:40 +0530

    datatypes in JS

---
+++ datatypes.js
@@ -0,0 +1,21 @@
+let age = 19;
+let price = 99.99;
+
+let fullName = "tony";
+
+let x = null;
+
+let y = BigInt("123");
+
+let z = Symbol("Hello!");
+
+// declaring object//
+
+const student = {
+    nam_e : "Ben 10",
+    age :20,
+    cpga : 9.3,
+    isPass : true
+}
+
+console.log(student.nam_e);   //Ben 10//
