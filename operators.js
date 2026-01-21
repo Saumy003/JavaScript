05c4f176eaa3276d501971c79d7397188d13948f
Saumy003 <saumysingh2005@gmail.com>
2026-01-21 23:38:10 +0530

    operaters in js

---
+++ operators.js
@@ -0,0 +1,17 @@
+// Artimetic Operatores
+let a = 5;
+let b = 2;
+
+console.log("a + b =" , a + b);
+console.log("a - b =" , a - b);
+console.log("a * b =" , a * b);
+console.log("a / b =" , a / b);
+console.log("a % b =" , a % b);
+console.log("a ** b =" , a ** b); // 5^2
+
+// Unary operators
+let c = 5;
+let d = 2;
+console.log("a = " , a & "b = ");
+a++ ; //6
+console.log("a = " , a);  //6
