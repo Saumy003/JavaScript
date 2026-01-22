6be2551c3d0974f71cdb03512ad082048a75feb1
Saumy003 <saumysingh2005@gmail.com>
2026-01-22 23:47:45 +0530

    conditional_statements

---
+++ conditional.js
@@ -0,0 +1,13 @@
+// if-statement
+
+let color = "green";
+
+if (color == "green") {
+    console.log("GO");
+}
+if (color == "yellow") {
+    console.log("READY TO GO");
+}
+if (color == "red") {
+    console.log("STOP");
+}
