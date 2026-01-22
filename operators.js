54806b09c07e7930b80ec0d4dfb10f88d10ecab5
Saumy003 <saumysingh2005@gmail.com>
2026-01-22 23:31:09 +0530

    operators in JS

--- operators.js
+++ operators.js
@@ -12,6 +12,36 @@ console.log("a ** b =" , a ** b); // 5^2
 // Unary operators
 let c = 5;
 let d = 2;
-console.log("a = " , a & "b = ");
-a++ ; //6
-console.log("a = " , a);  //6
+console.log("c = " , a & "d = ");
+c++ ; //6
+console.log("c = " , c);  //6
+
+// Assignment operator
+let x = 9;
+let y = 4;
+
+x += 3;  // x = x + 3
+console.log("x = " , x);
+
+// Comparision Operator
+let p = 2;
+let q = 7;
+
+console.log("p==q" , p==q);
+console.log("p!=q" , p!=q);
+
+// Logical operator
+
+let r = 6;
+let s = 5;
+
+let cond1 = r > s;  // true
+let cond2 = r === 6;  // false
+console.log("cond1 && cond2" , cond1 && cond2);
+console.log("cond1 || cond2" , cond1 || cond2);
+
+// ternary operator
+let age = 25;
+
+let result = age>=18 ? "adult" : "not adult" ;
+console.log(result);
