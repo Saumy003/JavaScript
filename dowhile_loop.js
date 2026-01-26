c06a5a9c82097b3ccf56411ab4a3136869db1c08
Saumy003 <saumysingh2005@gmail.com>
2026-01-26 21:40:03 +0530

    do-while in JS

---
+++ dowhile_loop.js
@@ -0,0 +1,14 @@
+// do-while loop
+
+let i = 20;
+do {
+    console.log("apna college");
+    i++;
+} while (i<=10);                  // loop will execute for one time only.
+
+
+let j = 20;
+do {
+    console.log("APPLE");
+    j++;
+} while (j<=40); 
