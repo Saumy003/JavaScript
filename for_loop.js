b96f698870b9bdbd4648395630636f40e67c7843
Saumy003 <saumysingh2005@gmail.com>
2026-01-26 21:38:19 +0530

    for loop in JS

---
+++ for_loop.js
@@ -0,0 +1,16 @@
+// for loop
+
+for (let i=1; i <= 5; i++) {
+    console.log("javascript");
+}
+
+for (let count=1; count <= 4; count ++) {
+    console.log("apna college")
+}
+
+// calculate sum from 1 to n= 5 
+let sum = 0;
+for (let i =1; i <= 5; i++) {
+    sum = sum + i;
+}
+console.log("sum=" , sum);
