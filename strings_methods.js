646fb71e21dc41f37eb6883122478845f7a9d119
Saumy003 <saumysingh2005@gmail.com>
2026-01-29 19:31:18 +0530

    string methods in JS

---
+++ strings_methods.js
@@ -0,0 +1,13 @@
+// Strings Methods
+
+// 1. str.toUpperCase()
+let str = "apna college";
+let newStr = str.toUpperCase();
+console.log(str);
+console.log(newStr);
+
+// 2. str.toLowerCase()
+
+// 3. str.trim()
+let str1 = "     apna college JS       ";
+console.log(str1.trim());
