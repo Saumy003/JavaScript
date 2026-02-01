312d5c5d0bae0c2187ab168412653834e8adebc7
Saumy003 <saumysingh2005@gmail.com>
2026-02-01 23:04:41 +0530

    practice set 3 in JS

--- practice3.js
+++ practice3.js
@@ -1,1 +1,9 @@
 // Ques on strings
+
+/*Q1. Prompt the user to enter thier full name. Generate the usename on the basis of output
+start username with @ and followed by their full name and ending with the fullname length*/
+
+let fulname = prompt("enter your full name");
+
+let username = "@" + fulname + fulname.length;
+console.log(username);
