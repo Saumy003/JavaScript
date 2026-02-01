9981cebcec73464e0dacddc20b7a9fcd46d75951
Saumy003 <saumysingh2005@gmail.com>
2026-02-01 23:07:33 +0530

    strings method in JS

--- strings_methods.js
+++ strings_methods.js
@@ -11,3 +11,23 @@ console.log(newStr);
 // 3. str.trim()
 let str1 = "     apna college JS       ";
 console.log(str1.trim());
+
+// 4. str.slice(start,end?)
+
+let num = "1347507";
+console.log(num.slice(2-6));  //7507
+
+// 5. str1.concat(str2)
+
+let str2 = "virat";
+let str3 = "kholi";
+console.log(str2.concat(str3));  // console.log(virat + kholi)
+
+// str.replace(searchVal , newVar)
+
+let str4 = "hello";
+console.log(str4.replace("h" , "m"));
+
+// str.charAt(val of index)
+let str5 = "IloveJS";
+console.log(str5.charAt(3));
