dee182ccea6517bc3274b73e0ca5b1a592a980e8
Saumy003 <saumysingh2005@gmail.com>
2026-02-04 16:55:18 +0530

    array in JS

--- array.js
+++ array.js
@@ -2,3 +2,22 @@
 
 let heroes = ["iron-man","hulk","spider-man" , "thor"];
 console.log(heroes);
+
+// array indices
+let marks = [74, 78, 89, 68, 47];
+marks[2] = 66;
+console.log(marks);
+
+// looping over an array
+let arr = [9,8,7,6,5,4,3];
+
+for (let idx = 0; idx < arr.length; idx++) {
+    console.log(arr[idx]);
+}
+
+// for of loop to print el of array
+let cities = ["pune", "mumbai", "delhi", "mohali", "kolkata"];
+
+for (let city of cities) {
+    console.log(city);
+}
