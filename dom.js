3469762e0c9ecee54174c426505aa6861fe29401
Saumy003 <saumysingh2005@gmail.com>
2026-02-10 23:06:55 +0530

    Document Object Model

---
+++ dom.js
@@ -0,0 +1,8 @@
+console.log("hello!");         // hello!
+// console.log(window)
+window.console.log("hello!!");  // hello!!
+window.alert("hii");
+
+// doc obj model
+console.dir(document.body);
+console.log(document.body);
