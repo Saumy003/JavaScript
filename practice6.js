2bb1a5fc44bd4b70b4f6a6bff762b233703d28a4
Saumy003 <saumysingh2005@gmail.com>
2026-02-25 23:01:25 +0530

    practice set on classes_objects in JS

--- practice6.js
+++ practice6.js
@@ -1,1 +1,23 @@
-// practice set 6 in javascript //
+/* practice set 6 in javascript 
+Classes & Objects */
+
+/* You are creating a website for your college. Create a class USER with 2 
+properties, name & email. I also has method called viewdata() that allow user to view
+website data */
+
+let DATA = "secret information"
+
+class user {
+    constructor(name , email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    viewData() {
+        console.log("data=" , DATA)
+    }
+
+}
+
+let student1 = new user("thor" , "thor@gmail.com");
+let student2 = new user("iron-man" , "ironman@gamil.com");
