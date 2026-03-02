caf5cfb4f08b9fcd38270c81687b89869ef98b10
Saumy003 <saumysingh2005@gmail.com>
2026-03-02 22:33:14 +0530

    async program in JS

---
+++ async_await.js
@@ -0,0 +1,13 @@
+// Asynchronous in JS // 
+
+console.log("one");
+console.log("two");
+
+setTimeout(() => {
+    console.log("hello");
+}, 4000);  // timeout , 4sec === 4000 milli sec
+
+console.log("three")
+console.log("four")
+
+
