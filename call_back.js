00ceb1b8cd8212f9ca662dc7bad1e61fec2bf377
Saumy003 <saumysingh2005@gmail.com>
2026-03-03 13:34:59 +0530

    callback in JS

--- call_back.js
+++ call_back.js
@@ -1,2 +1,21 @@
-// callback in JS //
+// Callback in JS //
+
+function sum(a , b) {
+    console.log(a + b);
+}
+
+function calculator(a , b, sumCallback) {
+    sumCallback(a , b);
+}
+
+calculator(1 , 2 , sum);   // sum function is passed as argument to calculator function //
+
+
+// Callback //
+
+function getData(dataId) {
+    setTimeout(() => {
+        console.log("Saumy Kumar" , dataId);
+    } , 2000);
+}
 
