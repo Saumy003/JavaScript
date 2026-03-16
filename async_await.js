4b6845cdbcb3afe8b1752974f47af7575fee3d86
Saumy003 <saumysingh2005@gmail.com>
2026-03-16 23:58:40 +0530

    async & await keywords

--- async_await.js
+++ async_await.js
@@ -1,4 +1,4 @@
-// Asynchronous in JS // 
+// Asynchronous program in JS // 
 
 console.log("one");
 console.log("two");
@@ -11,3 +11,50 @@ console.log("three")
 console.log("four")
 
 
+// async keyword & await keyword//
+
+// Exampl 1:-
+function  api() {
+return new Promise((resolve , reject) =>{
+    setTimeout(() => {
+        console.log("Weather data");
+        resolve(200);
+    } , 2000);
+});
+}
+
+async function getWeatherdata() {
+    await api();      //1st
+    await api();      //2nd
+}
+
+// Example 2:-
+function getData(dataId) {
+    return new Promise((resolve , reject) => {
+        setTimeout(() => {
+            console.log("data" , dataId);
+            resolve("success")
+        } , 4000);
+    });
+}
+
+async function getDataFunc() {
+    console.log("getting data 1...");
+    await getData(1);
+    console.log("getting data 2...");
+    await getData(2);
+    console.log("getting data 3...");
+    await getData(3);
+}
+
+/* IIFE function that is called immediately as soon as it is defined. 
+Can we used once only. Below is the syntax of IIFE. */
+
+// (async function () {
+//     console.log("getting data 1...");
+//     await getData(1);
+//     console.log("getting data 2...");
+//     await getData(2);
+//     console.log("getting data 3...");
+//     await getData(3);
+// }) ();
